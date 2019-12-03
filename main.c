70bd7803ca1773bc15f117aa3d2f660fb37fe59d
Marcos Sousa <marcosvinyciusss@gmail.com>
2019-12-03 20:30:09 -0300

    Atualizando códigos

--- main.c
+++ main.c
@@ -4,7 +4,10 @@
 int main(int argc, char const *argv[])
 {
     FILE *fp;
-    char linha[100];
+    char linha[1000];
+    char title[] = "<title>";
+    char fechar[] = "</";
+    char titulo[1000];
 
     fp = fopen("news.xml", "r");
 
@@ -12,7 +15,38 @@ int main(int argc, char const *argv[])
     {
         while (fgets(linha, sizeof(linha), fp))
         {
-            
+            int i = 0;
+            int k = 0;
+            for (i = 0; i < 100; i++)
+            {
+                if (linha[i] == title[k])
+                {
+                    k++;
+                    if (k == strlen(title))
+                    {
+                        int t = 0;
+                        for (int j = k; j < strlen(linha); j++)
+                        {
+                            char ch = linha[j];
+                            char ch2 = linha[j+1];
+                            if (ch != '<' && ch2 != '/')
+                            {
+                                titulo[t] = linha[j];
+                            }
+                            if (ch == '<' && ch2 == '/')
+                            {
+                                titulo[t] = '\0';
+                                break;
+                            }
+                            t++;
+                        }
+                        printf("%s\n", titulo);
+                    } 
+                } else
+                {
+                    k = 0;
+                }
+            }   
         }
     }
     
